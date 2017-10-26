89d8265f07d5224de91d818c2d9d6cec9ea592d1
ufgtb24 <1026773987@qq.com>
2017-10-26 14:41:34 +0800

    删除DataManager,直接使用BatchGenerator,能够分批载入

--- dataRelated.py
+++ dataRelated.py
@@ -2,25 +2,100 @@ import numpy as np
 import SimpleITK as sitk
 import os
 import pickle
-
+import random
 
 
 class BatchGenerator(object):
-    def __init__(self,batch_size,box,y):
+    def __init__(self,data_config):
+
+        self.shape_box = data_config.shape_box
+        self.box_train=None
+        self.total_case_dir=data_config.total_case_dir
+        self.case_dir_list=os.listdir(self.total_case_dir)
+        self.total_case_num=len(self.case_dir_list)
+        self.load_case_once=data_config.load_case_once
+        self.switch_data_internal=data_config.switch_data_internal
+        self.world_to_cubic=data_config.world_to_cubic
+        self.batch_size=data_config.batch_size
         self.index=0
-        box.shape=list(box.shape)+[1]
-        self.box=box
-        self.y=y
+        self.data_used_curcle=0
+        self.load_case()
 
-        self.batch_size=batch_size
-        self.sample_num=box.shape[0]
+    def load_case(self):
+        sample_dir_list = random.sample(self.case_dir_list, self.load_case_once)
+        box_list=[]
+        y_list=[]
+        for case_dir in sample_dir_list:
+            full_case_dir=self.total_case_dir+'\\'+case_dir
+            box_list.append(self.loadmhds(full_case_dir))
+            y_list.append(self.load_y(full_case_dir+'\\info.txt'))
+
+        self.box=np.expand_dims(np.concatenate(box_list,axis=0),axis=4)
+        self.y=np.concatenate(y_list,axis=0)
+        self.sample_num=self.y.shape[0]
 
 
 
+
+    def load_y(self, info_file):
+        info = np.reshape(np.loadtxt(info_file), [-1, 9])
+
+        origin = np.reshape(info[:, :3], [-1, 3])
+        origin = np.reshape(np.tile(origin, np.array([2])), [-1, 3])
+        target = np.reshape(info[:, 3:], [-1, 3])
+        target = np.reshape((target - origin) * self.world_to_cubic, [-1, 6]).astype(np.int32)
+        return target
+
+    def load_itk(self, filename):
+        # Reads the image using SimpleITK
+        itkimage = sitk.ReadImage(filename)
+        # Convert the image to a  numpy array first and then shuffle the dimensions to get axis in the order z,y,x
+        ct_scan = sitk.GetArrayFromImage(itkimage)
+
+        return ct_scan
+
+    def loadpickles(self, collection_path):
+        box_list = []
+        for fileName in os.listdir(collection_path):
+            if os.path.splitext(fileName)[1] == '.pickle':
+                toothPath = os.path.join(collection_path, fileName)
+                box_list.append(self.loadpickle(toothPath))
+        box = np.stack(box_list)
+        box.shape = [-1] + self.shape_box
+        return box
+
+    def loadpickle(self, path):
+        with open(path, 'rb') as file:
+            data = pickle.load(file)
+        return data
+
+    def savePickle(self, data, path):
+        with open(path, 'wb') as file:
+            pickle.dump(data, file)
+
+    def loadmhds(self, collection_path):
+        '''
+        :param collection_path: train或test路径，路径下包含多个病例
+        :return: [b,32,32,32]
+        '''
+
+        box_list = []
+        for fileName in os.listdir(collection_path):
+            if os.path.splitext(fileName)[1] == '.mhd':
+                toothPath = os.path.join(collection_path, fileName)
+                box_list.append(self.load_itk(toothPath))
+
+        box = np.stack(box_list)
+        box.shape = [-1] + self.shape_box
+        return box
 
     def get_batch(self):
         if self.index+ self.batch_size>self.sample_num:
+            if self.data_used_curcle > self.switch_data_internal:
+                self.load_case()
+                self.data_used_curcle = 0
             self.index=0
+            self.data_used_curcle+=1
             perm = np.arange(self.sample_num)
             np.random.shuffle(perm)  # 打乱
             self.box = self.box[perm]
@@ -37,109 +112,6 @@ class BatchGenerator(object):
 
 BOX_LEN=32
 
-class DataManager(object):
-    def __init__(self, data_config,
-                 test_box_path=None, test_info_file=None,
-                 train_box_path=None, train_info_file=None
-                 ):
-        self.shape_box = data_config.shape_box
-        self.box_train=None
-        need_train=False
-
-        if train_box_path!=None:
-            need_train=True
-            if data_config.format=='pickle':
-                self.box_train=self.loadpickles(train_box_path)
-            elif data_config.format=='mhd':
-                self.box_train=self.loadmhds(train_box_path)
-            self.y_train=self.load_y(train_info_file, data_config.world_to_cubic)
-
-
-        if data_config.format == 'pickle':
-            self.box_test = self.loadpickles(test_box_path)
-        elif data_config.format == 'mhd':
-            self.box_test = self.loadmhds(test_box_path)
-        self.y_test = self.load_y(test_info_file, data_config.world_to_cubic)
-
-        if need_train:
-            self.__trainGenerator = BatchGenerator(batch_size=data_config.batch_size_train,box=self.box_train,y= self.y_train)
-        self.__testGenerator = BatchGenerator(batch_size=data_config.batch_size_test,box=self.box_test, y=self.y_test )
-
-    def load_y(self,info_file, world_to_cubic):
-        info = np.reshape(np.loadtxt(info_file),[-1,9])
-
-        origin = np.reshape(info[:, :3], [-1, 3])
-        origin = np.reshape(np.tile(origin, np.array([2])), [-1, 3])
-        target = np.reshape(info[:, 3:], [-1, 3])
-        target = np.reshape((target - origin) * world_to_cubic, [-1, 6]).astype(np.int32)
-        return target
-
-    # def load_y(self, info_file, world_to_cubic):
-    #     info=np.loadtxt(info_file)
-    #     info.shape=[-1,9]
-    #     origin=info[:,:3]
-    #     target=info[:,3:]
-    #     x_index=[0,3]
-    #     y_index=[1,4]
-    #     z_index=[2,5]
-    #     target[:, x_index]=(target[:,x_index]-origin[:,0][:,np.newaxis])*world_to_cubic
-    #     target[:, y_index]=(target[:,y_index]-origin[:,1][:,np.newaxis])*world_to_cubic
-    #     target[:, z_index]=(target[:,z_index]-origin[:,2][:,np.newaxis])*world_to_cubic
-    #     target=target.astype(np.int32)
-    #     return target
-
-
-    def load_itk(self,filename):
-        # Reads the image using SimpleITK
-        itkimage = sitk.ReadImage(filename)
-        # Convert the image to a  numpy array first and then shuffle the dimensions to get axis in the order z,y,x
-        ct_scan = sitk.GetArrayFromImage(itkimage)
-
-        return ct_scan
-
-
-    def loadpickles(self,collection_path):
-        box_list = []
-        for fileName in os.listdir(collection_path):
-            if os.path.splitext(fileName)[1] == '.pickle':
-                toothPath = os.path.join(collection_path, fileName)
-                box_list.append(self.loadpickle(toothPath))
-        box = np.stack(box_list)
-        box.shape=[-1]+self.shape_box
-        return box
-
-    def loadpickle(self,path):
-        with open(path, 'rb') as file:
-            data = pickle.load(file)
-        return data
-
-
-    def savePickle(self, data,path):
-            with open(path, 'wb') as file:
-                pickle.dump(data, file)
-
-    def loadmhds(self, collection_path):
-        '''
-        :param collection_path: train或test路径，路径下包含多个病例
-        :return: [b,32,32,32]
-        '''
-
-        box_list = []
-        for fileName in os.listdir(collection_path):
-            if os.path.splitext(fileName)[1] == '.mhd':
-                toothPath = os.path.join(collection_path, fileName)
-                box_list.append(self.load_itk(toothPath))
-
-        box = np.stack(box_list)
-        box.shape=[-1]+self.shape_box
-        return box
-
-
-    def getTrainBatch(self):
-        return self.__trainGenerator.get_batch()
-
-    def getTestBatch(self):
-        return self.__testGenerator.get_batch()
 
 
 if __name__ == '__main__':
@@ -147,18 +119,14 @@ if __name__ == '__main__':
     class DataConfig(object):
         shape_box = [128, 128, 128]
         shape_crop = [64, 64, 64]
-        world_to_cubic = 0.1
+        world_to_cubic = 128 / 12.
         batch_size_train = 2
         batch_size_test = 1
+        total_case_dir = 'F:\\ProjectData\\Feature\\Tooth'
+        each_load_num = 10  # 每次读的病例数
+        switch_data_internal = 10  # 当前数据洗牌n次读取新数据
         need_Save = False
         need_Restore = False
         format = 'mhd'
 
 
-    root='F:\ProjectData\Retouch\\root'
-    DataManager(DataConfig,
-                train_box_path=os.path.join(root,'train'),
-                train_info_file=os.path.join(root,'train','info.txt'),
-                test_box_path=os.path.join(root,'test'),
-                test_info_file=os.path.join(root,'test','info.txt')
-                )
--- level_train.py
+++ level_train.py
@@ -3,7 +3,7 @@ import commen_structure as commen
 import os
 
 from CNN import CNN
-from dataRelated import DataManager
+from dataRelated import  BatchGenerator
 
 
 class Level(object):
@@ -22,30 +22,41 @@ class Level(object):
             update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
             with tf.control_dependencies(update_ops):
                 # Ensures that we execute the update_ops before performing the train_step
-                self.optimizer = commen.Optimizer(self.loss, initial_learning_rate=0.02,
+                self.optimizer = commen.Optimizer(self.loss, initial_learning_rate=0.1,
                                              max_global_norm=1.0).optimize_op
 
 class NetConfig(object):
     shape_box=[128,128,128]
-    channels = [1, 40, 60, 80]
+    channels = [1, 40, 60, 80,100]
     layer_num = len(channels) - 1
     fc_size = [512, 6]
 
 
-class DataConfig(object):
+class TrainDataConfig(object):
     shape_box=[128,128,128]
     shape_crop=[64,64,64]
     world_to_cubic=128/12.
-    batch_size_train = 2
-    batch_size_test = 1
-    need_Save = False
-    need_Restore = False
+    batch_size=4
+    total_case_dir='F:/ProjectData/Feature/Tooth'
+    load_case_once=10  #每次读的病例数
+    switch_data_internal=10 #当前数据洗牌n次读取新数据
+    format = 'mhd'
+
+class TestDataConfig(object):
+    shape_box=[128,128,128]
+    shape_crop=[64,64,64]
+    world_to_cubic=128/12.
+    batch_size=1
+    total_case_dir='F:/ProjectData/Feature/Tooth'
+    load_case_once=1  #每次读的病例数
+    switch_data_internal=1000 #当前数据洗牌n次读取新数据
     format = 'mhd'
 
 
 if __name__ == '__main__':
-    ROOT_PATH = 'F:/ProjectData/Feature'
     MODEL_PATH= 'F:/ProjectData/Feature/model/level_1'
+    NEED_RESTORE=False
+    NEED_SAVE=False
 
 
 
@@ -54,17 +65,13 @@ if __name__ == '__main__':
 
     saver = tf.train.Saver()
 
-    dataManager =DataManager(DataConfig(),
-                train_box_path=os.path.join(ROOT_PATH,'train'),
-                train_info_file=os.path.join(ROOT_PATH,'train','info.txt'),
-                test_box_path=os.path.join(ROOT_PATH,'test'),
-                test_info_file=os.path.join(ROOT_PATH,'test','info.txt')
-                )
+    train_batch_gen=BatchGenerator(TrainDataConfig)
+    test_batch_gen=BatchGenerator(TestDataConfig)
 
     with tf.Session() as sess:
         writer = tf.summary.FileWriter('log/', sess.graph)
 
-        if DataConfig.need_Restore:
+        if NEED_RESTORE:
             assert os.path.exists(MODEL_PATH+ 'checkpoint')  # 判断模型是否存在
             saver.restore(sess, MODEL_PATH + 'model.ckpt')  # 存在就从模型中恢复变量
         else:
@@ -73,23 +80,23 @@ if __name__ == '__main__':
 
         winner_loss = 10 ** 8
         step_from_last_mininum = 0
-        test_step = 1
+        test_step = 10
 
         for iter in range(10**8):
-            box_batch, y_batch=dataManager.getTrainBatch()
+            box_batch, y_batch=train_batch_gen.get_batch()
             feed_dict={level.box:box_batch, level.targets:y_batch,
                        level.phase:True,level.keep_prob:0.5}
             _,loss_train=sess.run([level.optimizer,level.loss],feed_dict=feed_dict)
             if iter % test_step==0:
                 step_from_last_mininum += 1
-                box_batch, y_batch = dataManager.getTestBatch()
+                box_batch, y_batch = test_batch_gen.get_batch()
                 feed_dict = {level.box: box_batch, level.targets: y_batch,
                              level.phase: False,level.keep_prob:1}
                 loss_test = sess.run(level.loss, feed_dict=feed_dict)
                 if loss_test<winner_loss:
                     winner_loss=loss_test
                     step_from_last_mininum=0
-                    if DataConfig.need_Save:
+                    if NEED_SAVE:
                         save_path = saver.save(sess, MODEL_PATH + 'model.ckpt')
 
                 print("%d  trainCost=%f   testCost=%f   winnerCost=%f   test_step=%d\n"
